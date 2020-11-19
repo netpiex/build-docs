f7463686fcf8bea932569e5d65353a7b0d8207cc
Pramrudee Aiumsupucgul <pookaium@gmail.com>
2020-11-19 10:27:13 +0700

    remove substitution extensions

--- config/netpie/conf_netpie.py
+++ config/netpie/conf_netpie.py
@@ -29,8 +29,6 @@ release = '1'
 extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.githubpages',
-    'sphinx-prompt',
-    'sphinx_substitution_extensions'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
